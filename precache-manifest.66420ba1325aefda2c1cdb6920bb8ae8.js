c164cbbab2d77797de465b57baf158223a32cb3c
Pie <ith.cslsec.10@gmail.com>
2020-07-18 20:15:56 +0200

    Deploy folder

---
+++ precache-manifest.66420ba1325aefda2c1cdb6920bb8ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2155e0a1ebd1c530972457eeac992e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cd62a1ca2b63e7e5fbf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5b6bf8edb69f8547a64",
+    "url": "/static/js/2.2295b48e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2295b48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd62a1ca2b63e7e5fbf",
+    "url": "/static/js/main.f7b72264.chunk.js"
+  },
+  {
+    "revision": "9a51352997dad6d3b7db",
+    "url": "/static/js/runtime-main.edcc50ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
